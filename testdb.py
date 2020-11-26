96659dcd2d5a483caa11d6c6499400893d3b880b
Vince249 <vincent.poupet@neuf.fr>
2020-11-26 09:27:13 +0100

    delete useless files

--- testdb.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('database_chat.db',check_same_thread=False)
-cursor = conn.execute("SELECT * FROM user WHERE USERNAME = 'to' AND PASSWORD = 'to' ")
-print(cursor.fetchone())
-
-
-conn.close()
